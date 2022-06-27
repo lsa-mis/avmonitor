60b760edcddc04d3ffd2745ea8ba60c2aa7c143e
Margarita Barvinok <brita@umich.edu>
2022-06-27 14:58:12 -0400

    add helpers to show room and device state

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -18,4 +18,82 @@ module ApplicationHelper
 		(state.key == "Online" && state.value == "false") || ( state.key == "Error State" && state.value == "true")
 	end
 	
+	def source_volume(room)
+		room_device = get_room_device(room)
+		if room_device.device_states.where(key: "Source Volume").last.present?
+			return room_device.device_states.where(key: "Source Volume").last.value
+		else
+			return ""
+		end
+	end
+
+	def current_source(room)
+		room_device = get_room_device(room)
+		if room_device.device_states.where(key: "Current Source").last.present?
+			source = room_device.device_states.where(key: "Current Source").last.value
+			if source != "0"
+				return room_device.device_states.where("key LIKE 'VideoSource%' AND key LIKE '%" + "#{source}'").last.value
+			else
+				return "not selected"
+			end
+		else 
+			return "not available"
+		end
+	end
+
+	def ceiling_mic(room)
+		room_device = get_room_device(room)
+		if room_device.device_states.where(key: "Ceiling Mic Signal").last.present?
+			return room_device.device_states.where(key: "Ceiling Mic Signal").last.value
+		else 
+			return "not available"
+		end
+	end
+
+	def wl_mic(room)
+		room_device = get_room_device(room)
+		if room_device.device_states.where(key: "WL Mic Signal").last.present?
+			return room_device.device_states.where(key: "WL Mic Signal").last.value
+		else 
+			return "not available"
+		end
+	end
+
+	def wl_mic_volume(room)
+		room_device = get_room_device(room)
+		if room_device.device_states.where(key: "Mic Volume").last.present?
+			return room_device.device_states.where(key: "Mic Volume").last.value
+		else 
+			return "not available"
+		end
+	end
+
+	def lamp_hour(device)
+		if device.device_states.where(key: "Lamp Hours").last.present?
+			return device.device_states.where(key: "Lamp Hours").last.value
+		else
+			return ""
+		end
+	end
+
+	def device_online(device)
+		if device.device_states.pluck(:key).include?("Online")
+			return device.device_states.where(key: "Online").last.value
+		else
+			return "-"
+		end
+	end
+
+	def device_power_is_on(device)
+		if device.device_states.pluck(:key).include?("Power Is On")
+			return device.device_states.where(key: "Power Is On").last.value
+		else
+			return "-"
+		end
+	end
+
+	def video_source_device?(device)
+		device.device_states.pluck(:key).include?("Power Is On")
+	end
+	
 end
