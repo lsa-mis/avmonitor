1f9f241f51136eefb6999facc54c467ac94116a2
Margarita Barvinok <brita@umich.edu>
2022-06-30 11:12:46 -0400

    remove unneded functions from a controller

--- app/javascript/controllers/autosubmit_controller.js
+++ app/javascript/controllers/autosubmit_controller.js
@@ -16,57 +16,8 @@ export default class extends Controller {
     }, 600)
   }
 
-  checkboxSubmit() {
-    clearTimeout(this.timeout)
-    this.timeout = setTimeout(() => {
-      this.statusTarget.textContent = 'Updating...'
-      Turbo.navigator.submitForm(this.formTarget)
-      this.sidebarTarget.classList.toggle('-translate-x-full')
-    }, 0)
-  }
-
-  capacitySubmit(event) {
-    clearTimeout(this.timeout)
-
-    this.timeout = setTimeout(() => {
-      var min_capacity = parseInt(this.min_capacityTarget.value)
-      var max_capacity = parseInt(this.max_capacityTarget.value)
-      if (min_capacity > max_capacity) {
-        this.capacity_errorTarget.classList.add("capacity-error--display")
-        this.capacity_errorTarget.classList.remove("capacity-error--hide")
-        this.capacity_errorTarget.innerText = "Min should be smaller than Max"
-      }
-      else {
-        this.capacity_errorTarget.classList.add("capacity-error--hide")
-        this.capacity_errorTarget.classList.remove("capacity-error--display")
-        this.capacity_errorTarget.innerText = ""
-
-        this.statusTarget.textContent = 'Updating...'
-        Turbo.navigator.submitForm(this.formTarget)
-        this.sidebarTarget.classList.toggle('-translate-x-full')
-      }
-    }, 600)
-  }
-
-  sortCapacity() {
-    clearTimeout(this.timeout)
-
-    this.timeout = setTimeout(() => {
-      this.statusTarget.textContent = 'Updating...'
-      Turbo.navigator.submitForm(this.formTarget)
-    }, 0)
-  }
-
-  change(event) {
-    event.preventDefault()
-    Turbo.navigator.submitForm(this.formTarget)
-  }
-
   clearFilters() {
     var url = window.location.pathname
-    // console.log(url)
     Turbo.visit(url)
-    // this.formTarget.reset()
-    // Turbo.navigator.submitForm(this.formTarget)
   }
 }
