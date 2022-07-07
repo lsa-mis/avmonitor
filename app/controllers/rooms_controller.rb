ed885d581dbe0b0bc7957517222d1ae5560d95de
Margarita Barvinok <brita@umich.edu>
2022-07-07 13:23:39 -0400

    remove destroy button and method

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -68,7 +68,6 @@ class RoomsController < ApplicationController
     
       respond_to do |format|
         if @room.save
-          format.turbo_stream 
           format.html { redirect_to room_url(@room), notice: "Room was successfully created." }
           format.json { render :show, status: :created, location: @room }
         else
@@ -91,7 +90,6 @@ class RoomsController < ApplicationController
     
     respond_to do |format|
       if @room.update(room_params)
-        format.turbo_stream
         format.html { redirect_to room_url(@room), notice: "Room was successfully updated." }
         format.json { render :show, status: :ok, location: @room }
       else
@@ -102,14 +100,14 @@ class RoomsController < ApplicationController
   end
 
   # DELETE /rooms/1 or /rooms/1.json
-  def destroy
-    @room.destroy
+  # def destroy
+  #   @room.destroy
 
-    respond_to do |format|
-      format.html { redirect_to rooms_url, notice: "Room was successfully destroyed." }
-      format.json { head :no_content }
-    end
-  end
+  #   respond_to do |format|
+  #     format.html { redirect_to rooms_url, notice: "Room was successfully destroyed." }
+  #     format.json { head :no_content }
+  #   end
+  # end
 
   private
     # Use callbacks to share common setup or constraints between actions.
