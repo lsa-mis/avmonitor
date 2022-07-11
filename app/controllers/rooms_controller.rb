46e78fb27dbbc99f4eb3156a4d47580232438484
Margarita Barvinok <brita@umich.edu>
2022-07-11 16:10:45 -0400

    remove comments

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -103,16 +103,6 @@ class RoomsController < ApplicationController
     end
   end
 
-  # DELETE /rooms/1 or /rooms/1.json
-  # def destroy
-  #   @room.destroy
-
-  #   respond_to do |format|
-  #     format.html { redirect_to rooms_url, notice: "Room was successfully destroyed." }
-  #     format.json { head :no_content }
-  #   end
-  # end
-
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_room
