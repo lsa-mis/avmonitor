ba8e049892726b227b42621c63369c418ffbe6fd
Margarita Barvinok <brita@umich.edu>
2022-07-19 07:49:48 -0400

    add pundid to application_controller.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,29 @@
 class ApplicationController < ActionController::Base
   include Pundit::Authorization
+
+  rescue_from Pundit::NotAuthorizedError, with: :user_not_in_group
+  before_action :set_membership
+  after_action :verify_authorized, unless: :devise_controller?
+
+  private
+
+  def user_not_authorized
+    flash[:alert] = "Please sign in to perform this action."
+    redirect_to(request.referrer || root_path)
+  end
+
+  def user_not_in_group
+    flash[:alert] = "You are not authorized to perform this action."
+    redirect_to about_path
+  end
+
+  def set_membership
+    if user_signed_in?
+      current_user.membership = session[:user_memberships]
+      current_user.admin = session[:user_admin]
+    else
+      new_user_session_path
+    end
+  end
+
 end
