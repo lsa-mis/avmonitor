37c2a0d3115007bba548465ba1fa14c0096540ba
Margarita Barvinok <brita@umich.edu>
2022-07-20 15:26:20 -0400

    aothorize dashboard

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,8 +1,10 @@
 class DashboardController < ApplicationController
   include ApplicationHelper
+  before_action :authenticate_user!
 
   def index
    rooms =  Room.active
-    @rooms = rooms_need_attention(rooms)
+   @rooms = rooms_need_attention(rooms)
+   authorize :dashboard
   end
 end
