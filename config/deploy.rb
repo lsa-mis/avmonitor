006140bc62383830b6b03de466b06283e7e9e6ac
Rick Smoke <rsmoke@umich.edu>
2022-07-28 12:24:57 -0400

    setup cap bits

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,8 +1,17 @@
 # config valid for current version and patch releases of Capistrano
 lock "~> 3.17.0"
 
-set :application, "my_app_name"
-set :repo_url, "git@example.com:me/my_repo.git"
+set :rbenv_type, :user
+set :rbenv_ruby, File.read('.ruby-version').strip
+set :rbenv_prefix, "RBENV_ROOT=#{fetch(:rbenv_path)} RBENV_VERSION=#{fetch(:rbenv_ruby)} #{fetch(:rbenv_path)}/bin/rbenv exec"
+set :rbenv_map_bins, %w{rake gem bundle ruby rails}
+set :rbenv_roles, :all # default value
+
+set :application, "avmonitor"
+set :repo_url, "git@github.com:lsa-mis/avmonitor.git"
+set :user, 'deployer'
+set :deploy_to, "/home/#{fetch(:user)}/apps/#{fetch(:application)}"
+set :tmp_dir, "/home/deployer/tmp" #"#{fetch(:home)}/tmp"
 
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
