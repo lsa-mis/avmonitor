7a242eec499ba3e131fed868ddc77eff3c1368a2
Rick Smoke <rsmoke@umich.edu>
2022-07-28 17:11:20 -0400

    add linked files

--- config/deploy.rb
+++ config/deploy.rb
@@ -40,7 +40,7 @@ set :keep_releases, 3
 # set :pty, true
 
 # Default value for :linked_files is []
-# append :linked_files, "config/database.yml", 'config/master.key'
+set :linked_files, "config/database.yml", 'config/master.key'
 
 # Default value for linked_dirs is []
 # append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "tmp/webpacker", "public/system", "vendor", "storage"
