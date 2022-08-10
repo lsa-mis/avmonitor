a8239fcd583b408bac24432a1bea52950abf1b93
Rick Smoke <rsmoke@umich.edu>
2022-08-10 13:13:04 -0400

    remove excess comments

--- lib/websocket_create.rb
+++ lib/websocket_create.rb
@@ -5,20 +5,18 @@ require_relative 'connect_socket'
 begin
   # connect to the MySQL server
   dbh = Mysql2::Client.new(host: "localhost", username: "root", password: "I9qACl7Jl3xobBZqQM", database: "avm_development")
-  # get server version string and display it
   results = dbh.query("SELECT websocket_ip, websocket_port, facility_id, tport FROM rooms")
+
   threads = []
   results.each do |row|
     puts "in loop"
     socket = "wss://" + row['websocket_ip'] + ":" + row['websocket_port']
-    # socket = "hello"
     name = row['facility_id']
     tport = row['tport']
     puts name
     puts socket
     puts tport
     wss_instance = ConnectSocket.new(name, socket, tport)
-    # app = wss_socket.connect
     threads << Thread.new { wss_instance.connect }
   end
 
