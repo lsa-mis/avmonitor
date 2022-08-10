784a99b5a3acdb8bfd5cbe578762e31362a64b81
Margarita Barvinok <brita@umich.edu>
2022-08-10 13:07:10 -0400

    remove unneeded files

--- lib/websocket_init/start_sockets.rb
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env ruby
-
-require 'typhoeus'
-# https://github.com/typhoeus/typhoeus
-
-socket_port = (8080..8090).to_a
-
-socket_port.each { |p| response = Typhoeus.get("http://localhost:#{p}/") }
-
