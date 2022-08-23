1672c35f62a662bd68cbd8d56e34d91d70559ce9
Rick Smoke <rsmoke@umich.edu>
2022-08-23 16:21:23 -0400

    swich sequence of calls

--- app/jobs/start_single_socket_job.rb
+++ app/jobs/start_single_socket_job.rb
@@ -6,7 +6,7 @@ class StartSingleSocketJob
   def perform(websocket_ip, websocket_port, facility_id, tport)
       socket = "wss://" + websocket_ip + ":" + websocket_port
       wss_instance = ConnectSocket.new(facility_id, socket, tport)
-      Thread.new { puts "Hello Ananta"}
-      Thread.new { wss_instance.create_socket }.join
+      Thread.new { wss_instance.create_socket }
+      Thread.new { puts "Hello Ananta"}.join
   end
 end
