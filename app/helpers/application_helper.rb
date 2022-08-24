6efb9e2e681c30137d0da1cdea73c0c00aa4e717
Margarita Barvinok <brita@umich.edu>
2022-08-24 15:27:37 -0400

    use current states to display data

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -18,12 +18,12 @@ module ApplicationHelper
     Device.where(room_id: room.id).where.not(name: 'Room').present?
   end
 
-  def room_has_device_states?(room)
+  def room_has_device_current_states?(room)
     devices = Device.where(room_id: room.id)
     state_exist = false
     catch :state do
       devices.each do |device|
-        if device.device_states.present?
+        if device.device_current_states.present?
           state_exist = true
           throw :state
         end
@@ -58,8 +58,8 @@ module ApplicationHelper
   
   def source_volume(room)
     room_device = get_room_device(room)
-    if room_device.device_states.where(key: "Source Volume").last.present?
-      return room_device.device_states.where(key: "Source Volume").last.value
+    if room_device.device_current_states.where(key: "Source Volume").last.present?
+      return room_device.device_current_states.where(key: "Source Volume").last.value
     else
       return ""
     end
@@ -67,8 +67,8 @@ module ApplicationHelper
 
   def ceiling_mic(room)
     room_device = get_room_device(room)
-    if room_device.device_states.where(key: "Ceiling Mic Signal").last.present?
-      return room_device.device_states.where(key: "Ceiling Mic Signal").last.value
+    if room_device.device_current_states.where(key: "Ceiling Mic Signal").last.present?
+      return room_device.device_current_states.where(key: "Ceiling Mic Signal").last.value
     else 
       return "not available"
     end
@@ -76,8 +76,8 @@ module ApplicationHelper
 
   def wl_mic(room)
     room_device = get_room_device(room)
-    if room_device.device_states.where(key: "WL Mic Signal").last.present?
-      return room_device.device_states.where(key: "WL Mic Signal").last.value
+    if room_device.device_current_states.where(key: "WL Mic Signal").last.present?
+      return room_device.device_current_states.where(key: "WL Mic Signal").last.value
     else 
       return "not available"
     end
@@ -85,32 +85,32 @@ module ApplicationHelper
 
   def wl_mic_volume(room)
     room_device = get_room_device(room)
-    if room_device.device_states.where(key: "Mic Volume").last.present?
-      return room_device.device_states.where(key: "Mic Volume").last.value
+    if room_device.device_current_states.where(key: "Mic Volume").last.present?
+      return room_device.device_current_states.where(key: "Mic Volume").last.value
     else 
       return "not available"
     end
   end
 
   def lamp_hour(device)
-    if device.device_states.where(key: "Lamp Hours").last.present?
-      return device.device_states.where(key: "Lamp Hours").last.value
+    if device.device_current_states.where(key: "Lamp Hours").last.present?
+      return device.device_current_states.where(key: "Lamp Hours").last.value
     else
       return ""
     end
   end
 
   def device_online(device)
-    if device.device_states.pluck(:key).include?("Online")
-      return device.device_states.where(key: "Online").last.value
+    if device.device_current_states.pluck(:key).include?("Online")
+      return device.device_current_states.where(key: "Online").last.value
     else
       return "-"
     end
   end
 
   def device_power_is_on(device)
-    if device.device_states.pluck(:key).include?("Power Is On")
-      return device.device_states.where(key: "Power Is On").last.value
+    if device.device_current_states.pluck(:key).include?("Power Is On")
+      return device.device_current_states.where(key: "Power Is On").last.value
     else
       return "-"
     end
@@ -118,17 +118,17 @@ module ApplicationHelper
 
   def video_source_device?(device)
     # check if a device in not a Touch panel. Tauch Panel doesn't have "Power Is On" key
-    device.device_states.pluck(:key).include?("Power Is On")
+    device.device_current_states.pluck(:key).include?("Power Is On")
   end
 
   def current_source(room, device)
     room_device = get_room_device(room)
     device_number = device.name.split(" ").last
     source_key = "Current Source " + device_number
-    if room_device.device_states.where(key: source_key).last.present?
-      source = room_device.device_states.where(key: source_key).last.value
-      if source != "0"
-        return room_device.device_states.where("`key` LIKE 'VideoSource%' AND `key` LIKE '%" + "#{source}'").last.value
+    if room_device.device_current_states.where(key: source_key).last.present?
+      source = room_device.device_current_states.where(key: source_key).last.value
+      if source != "0" && room_device.device_current_states.where("`key` LIKE 'VideoSource%' AND `key` LIKE '%" + "#{source}'").last.present?
+        return room_device.device_current_states.where("`key` LIKE 'VideoSource%' AND `key` LIKE '%" + "#{source}'").last.value
       else
         return "not selected"
       end
@@ -140,7 +140,7 @@ module ApplicationHelper
   def room_is_off?(room)
     device = Device.find_by(name: "Room", room_id: room.id)
     if device.present? && DeviceCurrentState.where(device_id: device.id).present?
-      if DeviceCurrentState.where(device_id: device.id).last.key == "Room Is On" && device.device_states.last.value == "false"
+      if DeviceCurrentState.where(device_id: device.id).last.key == "Room Is On" && device.device_current_states.last.value == "false"
         return true
       else
         return false
