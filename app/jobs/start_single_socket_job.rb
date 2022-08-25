7e42b0426998084546ce1be51fc6a546bbf0668f
Rick Smoke <rsmoke@umich.edu>
2022-08-25 08:16:56 -0400

    test various orders of calls to fix thread lock

--- app/jobs/start_single_socket_job.rb
+++ app/jobs/start_single_socket_job.rb
@@ -6,7 +6,8 @@ class StartSingleSocketJob
   def perform(websocket_ip, websocket_port, facility_id, tport)
       socket = "wss://" + websocket_ip + ":" + websocket_port
       wss_instance = ConnectSocket.new(facility_id, socket, tport)
-      Thread.new { wss_instance.create_socket }
-      Thread.new { puts "Hello Ananta"}.join
+      wss_instance.create_socket
+      # Thread.new { wss_instance.create_socket }
+      # Thread.new { puts "Hello Ananta"}.join
   end
 end
