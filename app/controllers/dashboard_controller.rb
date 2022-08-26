922837c5f715a684f48bb9f8d6da3acb379adb1d
Margarita Barvinok <brita@umich.edu>
2022-08-26 10:38:55 -0400

    move @rooms.. variable to helper to make it work when dashboard reloads without controller

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -3,8 +3,7 @@ class DashboardController < ApplicationController
   before_action :authenticate_user!
 
   def index
-   rooms =  Room.active
-   @rooms = rooms_need_attention(rooms)
    authorize :dashboard
   end
+
 end
