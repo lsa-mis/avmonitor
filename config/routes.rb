fa4469fb792702fb866982e0109388012affa7fd
Margarita Barvinok <brita@umich.edu>
2022-08-26 17:02:51 -0400

    solution for refreshing dashboard frame

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   get 'page/home'
   get 'dashboard', to: 'dashboard#index'
+  post 'dashboard', to: 'dashboard#index'
   devise_for :users, controllers: {omniauth_callbacks: "users/omniauth_callbacks", sessions: "users/sessions"} do
     delete 'sign_out', :to => 'users/sessions#destroy', :as => :destroy_user_session
   end
