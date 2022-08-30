192ed5a0ee3e7c9defbed5f51edb27e8a65edaff
Rick Smoke <rsmoke@umich.edu>
2022-08-30 10:59:04 -0400

    fix url for redis config

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,7 +1,7 @@
 Sidekiq.configure_server do |config|
-  config.redis = { url: 'redis://redis.example.com:7372/0' }
+  config.redis = { url: 'redis://lsa-avm-staging.miserver.it.umich.edu:7372/0' }
 end
 
 Sidekiq.configure_client do |config|
-  config.redis = { url: 'redis://redis.example.com:7372/0' }
+  config.redis = { url: 'redis://lsa-avm-staging.miserver.it.umich.edu:7372/0' }
 end
