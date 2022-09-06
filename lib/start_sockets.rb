d0bc70f21cc47423b36772bc45b732c31b72343d
Margarita Barvinok <brita@umich.edu>
2022-09-06 16:35:29 -0400

    always use sidekiq to start sockets

--- lib/start_sockets.rb
+++ lib/start_sockets.rb
@@ -1,6 +1,9 @@
 #!/usr/bin/env ruby
 require 'mysql2'
 require_relative 'connect_socket'
+require 'sidekiq'
+require_relative 'start_single_socket_job'
+
 
 begin
   # connect to the MySQL server
@@ -10,25 +13,22 @@ begin
     dbh = Mysql2::Client.new(host: "localhost", username: ENV["DBUSER"], password: ENV["DBPWD"], database: "avm_development" )
   end
   results = dbh.query("SELECT websocket_ip, websocket_port, facility_id, tport FROM rooms")
-
-  threads_create = []
-  # start dummy websocket
+  # start dummy socket
+  cmd_output = %x[lsof -i -P -n | grep LISTEN]
+    puts "cmd potput"
+    puts cmd_output
+    cmd_output = %x[lsof -i -P -n | grep LISTEN | grep 8999]
+    puts "cmd potput"
+    puts cmd_output
+    if cmd_output == ''
+      StartSingleSocketJob.perform_async("10.211.103.181", "32123", "dummy_room", "8999")
+    end
   results.each do |row|
-    socket = "wss://" + row['websocket_ip'] + ":" + row['websocket_port']
-    name = row['facility_id']
-    tport = row['tport']
-    wss_instance = ConnectSocket.new(name, socket, tport)
-    threads_create << Thread.new { wss_instance.create_socket }
-    puts "Thread created for #{name} - #{socket} - [#{tport}]"
+    StartSingleSocketJob.perform_async(row['websocket_ip'], row['websocket_port'], row['facility_id'], 
+    row['tport'])
   end
 
 rescue Exception => e
   puts "error raised"
   puts [e, e.backtrace].flatten.join("\n")
-end
-
-if threads_create.any?
-  threads_create.each(&:join)
-else 
-  puts "There are no threads"
 end
