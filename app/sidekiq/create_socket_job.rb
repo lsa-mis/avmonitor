7c44169c9f23aa761bcb202ce64939162a08cc1e
Rick Smoke <rsmoke@umich.edu>
2022-10-04 18:15:47 -0400

    remiove commented code

--- app/sidekiq/create_socket_job.rb
+++ app/sidekiq/create_socket_job.rb
@@ -6,12 +6,6 @@ class CreateSocketJob
     socket = "wss://" + websocket_ip + ":" + websocket_port
     wss_instance = WebsocketFactory.new(facility_id, socket)
     wss_instance.create_socket
-    # puts "$$$$$$$$$$$$$returned"
-    # wss_instance.wss.send("{'LSARoom': {'Password': 'LSAPassword'}}")
-
-    # puts "$$$$$$$$$$$$$class of wss: #{wss_instance.wss.class}"
-    # puts "$$$$$$$$$$$$$class of wss_methods: #{wss_instance.wss.methods}"
-    # puts "$$$$$$$$$$$$$tried to send to socket again"
   end
 
 
