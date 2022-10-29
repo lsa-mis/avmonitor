6e0cfa169cfe8792dc6d91c621f93fe45f04b5d4
Margarita Barvinok <brita@umich.edu>
2022-10-29 19:13:41 -0400

    add confirmation to change volume

--- app/javascript/controllers/sendtosocket_controller.js
+++ app/javascript/controllers/sendtosocket_controller.js
@@ -3,29 +3,39 @@ import { Controller } from "@hotwired/stimulus"
 export default class extends Controller {
   static targets = ["wl_mic_volume", "source_vol"]
 
-  // connect() {
-  //   this.element.textContent = "Hello World!"
-  // }
-
-  changeMicVolume() {
-    console.log("mic")
-    var volume = this.wl_mic_volumeTarget.value
-    console.log(volume)
-    var room_id = document.getElementById("room_id").value
-    console.log(room_id)
-    if (volume) {
-      fetch(`/send_to_room/${room_id}?operation=mic_vol&volume=${volume}`)
+  changeMicVolume(event) {
+    let confirmed = confirm("Are you sure?")
+    if (confirmed) {
+      console.log("mic")
+      var volume = this.wl_mic_volumeTarget.value
+      console.log(volume)
+      var room_id = document.getElementById("room_id").value
+      console.log(room_id)
+      if (volume) {
+        fetch(`/send_to_room/${room_id}?operation=mic_vol&volume=${volume}`)
+      }
+    }
+    else {
+      console.log("nothing")
+      event.preventDefault()
     }
   }
 
-  changeSourceVolume() {
-    console.log("source")
-    var volume = this.source_volTarget.value
-    console.log(volume)
-    var room_id = document.getElementById("room_id").value
-    console.log(room_id)
-    if (volume) {
-      fetch(`/send_to_room/${room_id}?operation=source_vol&volume=${volume}`)
+  changeSourceVolume(event) {
+    let confirmed = confirm("Are you sure?")
+    if (confirmed) {
+      console.log("source")
+      var volume = this.source_volTarget.value
+      console.log(volume)
+      var room_id = document.getElementById("room_id").value
+      console.log(room_id)
+      if (volume) {
+        fetch(`/send_to_room/${room_id}?operation=source_vol&volume=${volume}`)
+      }
+    }
+    else {
+      console.log("nothing")
+      event.preventDefault()
     }
   }
 }
