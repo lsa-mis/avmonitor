98d06de4a83aadfce0cdeba718b26668791d358e
Margarita Barvinok <brita@umich.edu>
2022-10-29 19:08:48 -0400

    add confirmation to change source

--- app/javascript/controllers/sendtosocket_controller.js
+++ app/javascript/controllers/sendtosocket_controller.js
@@ -3,14 +3,21 @@ import { Controller } from "@hotwired/stimulus"
 export default class extends Controller {
   static targets = ["source"]
 
-  changeSource() {
-    console.log("here")
-    var source_id = this.sourceTarget.value
-    console.log(source_id)
-    var room_id = document.getElementById("room_id").value
-    console.log(room_id)
-    if (source_id) {
-      fetch(`/send_to_room/${room_id}?operation=source_int&source=${source_id}`)
+  changeSource(event) {
+    let confirmed = confirm("Are you sure?")
+    if (confirmed) {
+      console.log("here")
+      var source_id = this.sourceTarget.value
+      console.log(source_id)
+      var room_id = document.getElementById("room_id").value
+      console.log(room_id)
+      if (source_id) {
+        fetch(`/send_to_room/${room_id}?operation=source_int&source=${source_id}`)
+      }
+    }
+    else {
+      console.log("nothing")
+      event.preventDefault()
     }
   }
 }
