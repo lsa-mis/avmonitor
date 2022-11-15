74893844f4e24fd0a9c03009b705318fef8ae9bd
Margarita Barvinok <brita@umich.edu>
2022-11-15 00:13:03 -0500

    put select source process into separate controller

---
+++ app/javascript/controllers/selectsource_controller.js
@@ -0,0 +1,38 @@
+import { Controller } from "@hotwired/stimulus"
+
+export default class extends Controller {
+  static values = {
+    name: String
+  }
+
+  connect() {
+    console.log("source")
+  }
+
+  changeSource(event) {
+    let confirmed = confirm("Are you sure?")
+    if (confirmed) {
+      console.log("here")
+      console.log(event.target.dataset)
+      var name = event.target.dataset.selectsourceNameValue
+      console.log(name)
+      var id = "source_" + name.replace(" ", "_")
+
+      var source = document.getElementById(id)
+      console.log("source:")
+      console.log(source)
+      var source_id = source.options[source.selectedIndex].value;
+      console.log(source_id)
+      var room_id = document.getElementById("room_id").value
+      console.log(room_id)
+      if (source_id) {
+        fetch(`/send_to_room/${room_id}?operation=source_int&source=${source_id}`)
+      }
+    }
+    else {
+      console.log("nothing")
+      location.reload()
+    }
+  }
+
+}
