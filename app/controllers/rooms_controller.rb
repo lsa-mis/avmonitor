7c29b49c7baa1a1890613bbe497605f6fca681f9
Margarita Barvinok <brita@umich.edu>
2022-12-09 12:19:46 -0500

    render listing in a better way

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -33,11 +33,16 @@ class RoomsController < ApplicationController
     end
     @rooms = @rooms.sort_by(&:facility_id)
     @room_types = Room.all.pluck(:room_type).uniq.sort
-    unless params[:q].nil?
-      render turbo_stream: turbo_stream.replace(
-      :roomListing,
-      partial: "rooms/listing"
-    )
+    # unless params[:q].nil?
+    #   render turbo_stream: turbo_stream.replace(
+    #   :roomListing,
+    #   partial: "rooms/listing"
+    # )
+    # end
+    if turbo_frame_request?
+      render partial: "listing"
+    else
+      render "index"
     end
   end
 
